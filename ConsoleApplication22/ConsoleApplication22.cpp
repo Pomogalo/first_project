0e38f590805d9fd3dd14a1da8514d1fc33a2bb41
Pomogalo <pomogalo2025@gmail.com>
2024-10-24 21:14:22 +0500

    Add files via upload
    Первый коммит

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+const double pi = 3.14;
+class square
+{
+private:
+    double r;
+    double p;
+    double res;
+public:
+    square(double r)
+    {
+        this->r = r;
+        this->p = pi;
+        this->res = res;
+    }
+    double dlina() {
+        res = 0;
+        res = (r * 2) * p;
+        return res;
+    }
+    double ploshad() {
+        res = 0;
+        res = p * (r * r);
+        return res;
+    }
+};
+
+int main()
+{
+    square sq12n{5.5};
+    cout << sq12n.ploshad();
+    cout << "\n" << sq12n.dlina();
+}
