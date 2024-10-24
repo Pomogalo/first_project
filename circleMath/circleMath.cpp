6a323dc312864ef3b9c5fdb5d60e4d633f9bf18a
Pomogalo <pomogalo2025@gmail.com>
2024-10-24 21:14:15 +0500

    Add files via upload
    Первый коммит

---
+++ circleMath/circleMath.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+#define PI 3.14
+class circle
+{
+    float radius;
+public:
+    circle(float& data) {
+        this->radius = data;
+    }
+
+    float getDlina()
+    {
+        return 2 * PI * this->radius;
+    }
+
+    float getPloshad()
+    {
+        return PI * (this->radius * this->radius);
+    }
+};
+int main()
+{
+    setlocale(0, "");
+
+    float rad;
+
+    cout << "Здравствуйте, введите пожалуйста радиус:";
+    cin >> rad;
+
+    circle circleClass{ rad };
+
+    cout << "Длина круга = "<< circleClass.getDlina() << endl;
+    cout << "Площадь круга = " << circleClass.getPloshad();
+}
+
